916bb300a65307f2bcae85e88bba2a63eb8de7f3
arid0910 <arid0910@g.educaand.es>
2024-11-28 12:23:51 +0100

    Plantilla react

---
+++ 1º Trim/HLC/React/plantilla/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <Button>
+        Dale
+      </Button>
+    </div>
+  );
+}
+
+export default App;
