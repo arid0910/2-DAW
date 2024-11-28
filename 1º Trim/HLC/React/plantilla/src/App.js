7b8a684fb2ffdc7209a0b38d136a7e1b294ed76b
arid0910 <arid0910@g.educaand.es>
2024-11-28 12:46:48 +0100

    React Ejr tresbotones

--- 1º Trim/HLC/React/plantilla/src/App.js
+++ 1º Trim/HLC/React/plantilla/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <Button>
-        Dale
-      </Button>
+      <Button>Dale</Button>
     </div>
   );
 }
