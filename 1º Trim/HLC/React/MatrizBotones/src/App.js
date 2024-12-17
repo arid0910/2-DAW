07bf11ec8521a7105734558f1eddac2ed580e5a6
arid0910 <arid0910@g.educaand.es>
2024-12-17 10:04:50 +0100

    React matriz botones empezado

---
+++ 1º Trim/HLC/React/MatrizBotones/src/App.js
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import { Component } from 'react'
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+export default App;
