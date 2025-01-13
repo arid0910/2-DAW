b4817b5d71f51d5174b2930234def52bd64d9659
arid0910 <arid0910@g.educaand.es>
2025-01-13 14:09:47 +0100

    PHP Ejercicios servicos web act1 hecho

--- Proyectos/2Trim/Servicios_Web/Actividad1/servicios_rest/index.php
+++ Proyectos/2Trim/Servicios_Web/Actividad1/servicios_rest/index.php
@@ -12,14 +12,63 @@ $app->get('/productos', function(){
 });
 
 //B
-$app->get("/producto/{cod}", function($request){
+$app->get('/producto/{cod}', function($request){
     $cod = $request->getAttribute("cod");
     echo json_encode(obtener_producto($cod));
 });
 
 //C
-$app->post("/producto/insertar", function(){
-    
+$app->post('/producto/insertar', function($request){
+    $datos[]=$request->getParam("cod");
+    $datos[]=$request->getParam("nombre");
+    $datos[]=$request->getParam("nombre_corto");
+    $datos[]=$request->getParam("descripcion");
+    $datos[]=$request->getParam("PVP");
+
+    echo json_encode(insertar_producto($datos));
+});
+
+//D
+$app->put('/producto/actualizar/{cod}',function($request){
+    $datos[]=$request->getParam("nombre");
+    $datos[]=$request->getParam("nombre_corto");
+    $datos[]=$request->getParam("descripcion");
+    $datos[]=$request->getParam("PVP");
+    $datos[]=$request->getAttribute("cod");
+
+    echo json_encode(actualizar_producto($datos));
+});
+
+//E
+$app->delete('producto/borrar/{cod}', function($request){
+    $cod=$request->getAttribute("cod");
+
+    echo json_encode(borrar_producto($cod));
+});
+
+//F 
+$app->get('/familias',function(){
+    echo json_encode(obtener_productos());
+});
+
+//G
+$app->get('repetido/{tabla}/{columna}/{valor}', function($request){
+    $tabla=$request->getAttribute("tabla");
+    $columna=$request->getAttribute("columna");
+    $valor=$request->getAttribute("valor");
+
+    echo json_encode(repetido_insertando($tabla, $columna, $valor));
+});
+
+//H
+$app->get('repetido/{tabla}/{columna}/{valor}/{columna_id}/{valor_id}', function($request){
+    $tabla=$request->getAttribute("tabla");
+    $columna=$request->getAttribute("columna");
+    $valor=$request->getAttribute("valor");
+    $columna_id=$request->getAttribute("columna_id");
+    $valor_id=$request->getAttribute("valor_id");
+
+    echo json_encode(repetido_editando($tabla, $columna, $valor, $columna_id, $valor_id));
 });
 
 $app->run();
