f9a04e425ec5e738cc6668252ab27d8f10d0c603
arid0910 <arid0910@g.educaand.es>
2025-01-14 10:13:43 +0100

    React login + hooks

---
+++ 1º Trim/DWEC/React/Login_hooks/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+import { Component } from 'react'
+import AppLogin from './Componentes/AppLogin';
+import Menu from './Componentes/Menu'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menuItem: "UNO",
+      logged: false,
+    }
+  }
+
+  changeMenu(item) {
+    this.setState({
+      menuItem: item
+    })
+  }
+
+  render() {
+    let obj = <Menu changeMenu={(item) => this.changeMenu(item)} />
+
+    if (!this.state.logged) {
+      obj = <AppLogin userLogin={(this.state.menuItem)} changeMenu={(item) => this.changeMenu(item)} />
+    }
+
+    return (
+      <div className="App">
+        <Menu menuItem={this.state.menuItem} changeMenu={(item) => this.changeMenu(item)} />
+        {obj}
+      </div>
+    );
+  }
+}
+export default App;
