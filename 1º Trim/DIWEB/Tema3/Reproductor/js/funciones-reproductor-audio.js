280c7cec7fdc707f99a2581b2acf08c670bb24ad
arid0910 <arid0910@g.educaand.es>
2025-01-15 13:59:26 +0100

    DIWEB repdroductor audio comprobar

--- 1º Trim/DIWEB/Tema3/Reproductor/js/funciones-reproductor-audio.js
+++ 1º Trim/DIWEB/Tema3/Reproductor/js/funciones-reproductor-audio.js
@@ -6,17 +6,17 @@ var restart = document.getElementById('restart');
 var volumeDown = document.getElementById('volume-down');
 var volumeUp = document.getElementById('volume-up');
 var isPaused = false;
-
+ 
 
 reproducir.onclick = function (ev) {
     if (!isPaused) {
         audio.play();
-        btnPlay.classList.remove("fa-solid fa-play");
-        btnPlay.classList.add("fa-solid fa-pause");
+        btnPlay.classList.remove("fa-solid", "fa-play");
+        btnPlay.classList.add("fa-solid", "fa-pause");
     } else {
         audio.pause();
-        btnPlay.classList.remove("fa-solid fa-pause");
-        btnPlay.classList.add("fa-solid fa-play");
+        btnPlay.classList.remove("fa-solid", "fa-pause");
+        btnPlay.classList.add("fa-solid", "fa-play");
     }
 
     isPaused = !isPaused;
@@ -25,16 +25,16 @@ reproducir.onclick = function (ev) {
 parar.onclick = function (ev) {
     audio.pause();
     audio.currentTime = 0;
-    btnPlay.classList.remove("fa-solid fa-pause");
-    btnPlay.classList.add("fa-solid fa-play");
+    btnPlay.classList.remove("fa-solid", "fa-pause");
+    btnPlay.classList.add("fa-solid", "fa-play");
     isPaused = false;
 };
 
 restart.onclick = function (ev) {
     audio.currentTime = 0;
     audio.play();
-    btnPlay.classList.remove("fa-solid fa-play");
-    btnPlay.classList.add("fa-solid fa-pause");
+    btnPlay.classList.remove("fa-solid", "fa-play");
+    btnPlay.classList.add("fa-solid", "fa-pause");
     isPaused = true;
 };
 
